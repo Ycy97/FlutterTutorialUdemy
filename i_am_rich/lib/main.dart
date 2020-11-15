401f1424269f0f0734a96c3305604a2d8b6ab6f0
ChengYee <chengyeeyap97@gmail.com>
2020-11-15 21:18:52 +0800

    I am Rich app completed with network image and asset image along with app icon for Android and iOS

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -12,8 +12,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://i.pinimg.com/originals/46/3d/87/463d87729ce7bea953f1995246d4320d.png'),
+            image: AssetImage('images/jeanneAlter.jpg'),
           ),
         ),
       ),
